9eb3a34a3d2009c916aa0f711cce44e34ab03837
Norman Bernard <nksb414@gmail.com>
2021-05-07 03:52:09 +0000

    completed question array of objects for inquirer prompts

--- index.js
+++ index.js
@@ -6,8 +6,54 @@ const questions = [
     {
       type: 'input',
       message: 'What is the project title?',
-      name: 'projectTitle'
-    }
+      name: 'projectTitle',
+    },
+    {
+      type: 'input',
+      message: 'Please provide a description for this project.',
+      name: 'projectDescription',
+    },
+    {
+      type: 'input',
+      message: 'Please provide installation instructions for this project.',
+      name: 'projectInstallationInstructions',
+    },
+    {
+        type: 'input',
+        message: 'Please provide usage information for this project.',
+        name: 'projectUsage',
+      },
+      {
+        type: 'input',
+        message: 'Who are the contributors of this project?',
+        name: 'projectContributors',
+      },
+      {
+        type: 'input',
+        message: 'Please provide test instructions for this project.',
+        name: 'projectTest',
+      },
+      {
+        type: 'checkbox',
+        message: 'Please select the license type that applies to this project.',
+        name: 'projectLicense',
+        choices: ['MIT', 'GNU Gplv3', 'Other'],
+      },
+      {
+        type: 'input',
+        message: 'What is your github username?',
+        name: 'projectGitUserName',
+      },
+      {
+        type: 'input',
+        message: 'Please provide a link to your github profile.',
+        name: 'projectGitHubProfile',
+      },
+      {
+        type: 'input',
+        message: 'What is your email address.',
+        name: 'projectEmailAddress',
+      },
 ];
 
 // TODO: Create a function to write README file
