bb2e4b7d939bb6093c0685290bd9a2fbb8b7eeec
Norman Bernard <nksb414@gmail.com>
2021-05-07 03:18:22 +0000

    added first question and ensured the resulting object would write to console log

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      message: 'What is the project title?',
+      name: 'projectTitle'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    const getReadmeDetails = inquirer.prompt(questions);
+    getReadmeDetails.then(response => {
+        console.log(response)
+    })
+}
 
 // Function call to initialize app
 init();
